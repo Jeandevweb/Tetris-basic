50ac57890834a01b541712101571c2c614792942
jean <jeandeveweb@gmail.com>
2021-12-13 19:04:06 +0100

    mise a niveau

--- app.js
+++ app.js
@@ -150,11 +150,11 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   
   function checkRotatedPosition(P){
-    P = P || currentPosition       //get current position.  Then, check if the piece is near the left side.
-    if ((P+1) % width < 4) {         //add 1 because the position index can be 1 less than where the piece is (with how they are indexed).     
-      if (isAtRight()){            //use actual position to check if it's flipped over to right side
-        currentPosition += 1    //if so, add one to wrap it back around
-        checkRotatedPosition(P) //check again.  Pass position from start, since long block might need to move more.
+    P = P || currentPosition       
+    if ((P+1) % width < 4) {              
+      if (isAtRight()){            
+        currentPosition += 1    
+        checkRotatedPosition(P) 
         }
     }
     else if (P % width > 5) {
